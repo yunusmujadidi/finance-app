bb3e6e73f0e38f66581f361166102811bef0a323
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-06-16 22:32:23 +0700

    setup project

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <p>authenthicated route</p>
+      Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
