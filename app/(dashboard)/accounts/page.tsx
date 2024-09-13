fff43770996364f2d1ede6bb17efc11325c55da4
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-09-14 00:04:41 +0700

    feat: add pagination, sorting, and filtering on table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -35,7 +35,7 @@ const AccountsPage = () => {
           </Button>
         </CardHeader>
         <CardContent>
-          <DataTable columns={columns} data={data} />
+          <DataTable filterKey="email" columns={columns} data={data} />
         </CardContent>
       </Card>
     </div>
