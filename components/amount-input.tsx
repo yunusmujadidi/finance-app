b93f6ae18db90a63f8bc9d72f252fbb5e1522414
Ahmad Yunus <yunusmujadidi@gmail.com>
2024-09-18 03:05:52 +0700

    feat: change format currency to IDR

--- components/amount-input.tsx
+++ components/amount-input.tsx
@@ -1,4 +1,4 @@
-import { cn } from "@/lib/utils";
+import { cn, formatCurrency } from "@/lib/utils";
 import { Input } from "./ui/input";
 import {
   Tooltip,
@@ -7,12 +7,13 @@ import {
   TooltipTrigger,
 } from "./ui/tooltip";
 import { Info, MinusCircle, PlusCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+
 export const AmountInput = ({
   defaultValue,
   disabled,
   value,
   placeholder,
-
   onChange,
 }: {
   defaultValue: any;
@@ -21,15 +22,41 @@ export const AmountInput = ({
   placeholder?: string;
   onChange: (value: string) => void;
 }) => {
+  const [displayValue, setDisplayValue] = useState("");
+
+  useEffect(() => {
+    if (value) {
+      const numericValue = Math.abs(parseFloat(value as string));
+      setDisplayValue(
+        formatCurrency(numericValue)
+          .replace(/^Rp\s?/, "")
+          .replace(/\./g, ".")
+      );
+    } else {
+      setDisplayValue("");
+    }
+  }, [value]);
+
   const parsedValue = parseFloat(value as string);
   const isIncome = parsedValue > 0;
   const isExpense = parsedValue < 0;
 
   const onReverseValue = () => {
     if (!value) return;
-
     onChange((parseFloat(value as string) * -1).toString());
   };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value.replace(/[^\d]/g, "");
+    const numericValue = parseInt(inputValue, 10);
+
+    if (!isNaN(numericValue)) {
+      onChange((isExpense ? -numericValue : numericValue).toString());
+    } else {
+      onChange("");
+    }
+  };
+
   return (
     <div className="relative">
       <TooltipProvider>
@@ -40,7 +67,6 @@ export const AmountInput = ({
               onClick={onReverseValue}
               className={cn(
                 "absolute bg-slate-400 hover:bg-slate-500 top-1.5 left-1.5 rounded-md p-2 flex items-center justify-center transition z-10",
-
                 isIncome && "bg-emerald-500 hover:bg-emerald-600",
                 isExpense && "bg-rose-500 hover:bg-rose-600"
               )}
@@ -50,21 +76,26 @@ export const AmountInput = ({
               {isExpense && <MinusCircle className="size-3 text-white" />}
             </button>
           </TooltipTrigger>
-          <TooltipContent>Use [+] or income and [-] for expense</TooltipContent>
+          <TooltipContent>
+            Use [+] for income and [-] for expense
+          </TooltipContent>
         </Tooltip>
       </TooltipProvider>
 
-      <Input
-        type="number"
-        className="pl-10"
-        defaultValue={defaultValue}
-        disabled={disabled}
-        placeholder={placeholder}
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-        max={2147483647}
-        min={-2147483648}
-      />
+      <div className="relative">
+        <span className="absolute left-10 top-1/2 -translate-y-1/2 text-gray-500">
+          Rp
+        </span>
+        <Input
+          type="text"
+          className="pl-16 pr-4"
+          defaultValue={defaultValue}
+          disabled={disabled}
+          placeholder={placeholder}
+          value={displayValue}
+          onChange={handleInputChange}
+        />
+      </div>
       <p className="text-xs mt-2">
         {isIncome && "This will count as income"}
         {isExpense && "This will count as expense"}
--- lib/utils.ts
+++ lib/utils.ts
@@ -6,8 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatCurrency(amount: number): string {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("id-ID", {
     style: "currency",
-    currency: "USD",
-  }).format(amount / 100);
+    currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(amount);
 }
